e74c6f343fbaaf42a96059334d11474ac55ecf49
sujin <sujinemilycho@gmail.com>
2020-05-19 16:07:22 +0900

    feat: Session을 이용한 Auth Form 실습

--- src/test/java/wooteco/subway/AuthAcceptanceTest.java
+++ src/test/java/wooteco/subway/AuthAcceptanceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import io.restassured.authentication.FormAuthConfig;
 import io.restassured.http.ContentType;
 import wooteco.subway.service.member.dto.MemberResponse;
 import wooteco.subway.service.member.dto.TokenResponse;
@@ -62,8 +63,13 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     }
 
     public MemberResponse myInfoWithSession(String email, String password) {
-        // TODO: form auth를 활용하여 /me/session 요청하여 내 정보 조회
-        return null;
+        return given()
+            .auth().form(email, password, new FormAuthConfig("/login", "email", "password"))
+            .accept(ContentType.JSON)
+            .when()
+            .get("/me/session")
+            .then().log().all()
+            .extract().as(MemberResponse.class);
     }
 
     public MemberResponse myInfoWithBearerAuth(TokenResponse tokenResponse) {
